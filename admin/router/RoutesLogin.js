8535ea72b028c9739480b9a3ce67b2de66411a71
Ulziimyagmar Ganbaatar <56241828+Ulziimyagmar@users.noreply.github.com>
2020-09-17 10:53:41 +0800

    layout and routes

---
+++ admin/router/RoutesLogin.js
@@ -0,0 +1,17 @@
+// import Login from "../components/auth/Login";
+// import NotFound from "../components/NotFound";
+// export default [
+//     {
+//         component: Login,
+//         path: '/',
+//         exact: true
+//     },
+//     {
+//         component: NotFound,
+//         path: '/not-found'
+//     },
+//     {
+//         component: NotFound,
+//         path: '*'
+//     },
+// ];
