948c2895f15dd523ed992e35fcc1007f3db053da
ialibuhk <82246494+ialibuhk@users.noreply.github.com>
2021-05-21 20:25:34 +0800

    dawd

--- front/components/Careers.js
+++ front/components/Careers.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import {
+	Container,
+	Row,
+	Col,
+	Table,
+	Card,
+	ListGroup,
+	ListGroupItem
+} from "react-bootstrap";
 import Header from "./include/Header";
 import Footer from "./include/Footer";
 import config from "../config";
@@ -18,22 +26,22 @@ class Careers extends Component {
 		config.get("ga").pageview(window.location.pathname);
 		window.scroll(0, 0);
 	}
-	
+
 	render() {
 		const {
 			jobPost: { jobposts, loadingJobPosts }
 		} = this.props;
 		console.log(jobposts);
 		const renderJobs = (jobposts) => {
-			return(
+			return (
 				<tr>
 					<td>{jobposts.opening}</td>
 					<td>{jobposts.requirements}</td>
 					<td>{jobposts.salary}</td>
 					<td>{jobposts.misc}</td>
 				</tr>
-			)
-		}
+			);
+		};
 		return (
 			<React.Fragment>
 				<Header location={this.props.location} />
@@ -53,19 +61,33 @@ class Careers extends Component {
 					</div>
 					<div className="careersposts">
 						<Container>
-							<Table responsive>
-								<thead>
-									<tr>
-										<th>opening</th>
-										<th>requirements</th>
-										<th>salary</th>
-										<th>misc</th>
-									</tr>
-								</thead>
-								<tbody>
-									{jobposts.map(renderJobs)}
-								</tbody>
-							</Table>
+							{loadingJobPosts ? (
+								<div>Loading</div>
+							) : (
+								<div className="CareersCards">
+									{jobposts.map((job) => {
+										return (
+											<div className="CareersCard">
+												<h2>{job.opening}</h2>
+												<h3>Шаардлага</h3>
+												<ul>
+													{job.requirements.map(
+														(rqr) => {
+															return <li>{rqr}</li>
+														}
+													)}
+												</ul>
+												<h3>Цалин</h3>
+												<p>{job.salary}</p>
+												<h3>Гүйцэтгэx үүрэг</h3>
+												<ul>{job.misc.map((msc)=>{
+													return <li>{msc}</li>
+												})}</ul>
+											</div>
+										);
+									})}
+								</div>
+							)}
 						</Container>
 					</div>
 				</div>
