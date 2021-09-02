283bba0b0a297d12082211c06ee7856fc9e51c49
Bulgantamir Lkhagvajav <79170631+bulgantamirl@users.noreply.github.com>
2021-09-02 12:44:28 +0800

    result single

--- front/reducers/testResultSingle.js
+++ front/reducers/testResultSingle.js
@@ -4,7 +4,8 @@ import {
 import config from "../config";
 const initialState = {
     loading: 1,
-    result: {}
+    result: {},
+    certified: false
 };
 
 export default(state = initialState, action) => {
@@ -12,15 +13,15 @@ export default(state = initialState, action) => {
         case getTestSingle.REQUEST:
             return {
                 ...state,
-                loading:1
+                loading: 1
             };
         case getTestSingle.RESPONSE:
             if(action.json.success) {
                 return {
                     ...state,
                     loading: 0,
-                    result: (action.json.result || {})
-                 
+                    result: (action.json.result || {}),
+                    certified: action.json.result?.result >=70 || false
                 };
             } else {
                 window.location.assign('/test/results');
