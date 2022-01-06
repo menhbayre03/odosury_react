45e567adfa418541b6aa16521e3ec9384c4768e0
Bulgantamir Lkhagvajav <79170631+bulgantamirl@users.noreply.github.com>
2022-01-06 20:21:46 +0800

    hhe

--- front/components/Contents.js
+++ front/components/Contents.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from "./include/Header";
+import ReactPlayer from "react-player";
 import Footer from "./include/Footer";
 import { Container, Row, Col, Button, Tabs, Tab, Accordion, Card, Spinner, Modal } from "react-bootstrap";
 import ReactStars from "react-rating-stars-component";
@@ -13,6 +14,7 @@ class Contents extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            index: 0
         };
     }
     componentDidMount() {
@@ -24,36 +26,152 @@ class Contents extends Component {
     render() {
         let discoveries = [
             {
-              title: "Fat Bunny - short film with Fat Rabbit - Big Bug Bunny",
-              description: "This is just a video. And if you want to know more about it etc...",
-              embed:"<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/axdOKdaepyE\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
-              created:"2022-01-04T04:28:08.220Z",
-              discoveryCategory:{
+                title: "Numb - Linkin Park",
+                description: "This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=kXYiU_JCYtU&ab_channel=LinkinPark',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
                 _id: "61d3ccd8c3f17d0cc42e8e29",
-                title: "fat bunny",
-              },
+                title: "numb",
+                },
+            },
+                {
+                title: "Three Days Grace - I Hate Everything About You",
+                description: "2 This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=d8ekz_CSBVg&ab_channel=ThreeDaysGraceVEVO',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
+                _id: "61d3ccd8c3f17d0cc42e8e29",
+                title: "2nd video",
+                },
             },
             {
-              title: "Fat Bunny - short film with Fat Rabbit - Big Bug Bunny",
-              description: "2 This is just a video. And if you want to know more about it etc...",
-              embed:"<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/axdOKdaepyE\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
-              created:"2022-01-04T04:28:08.220Z",
-              discoveryCategory:{
+                title: "Video 3",
+                description: "3 This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=kXYiU_JCYtU&ab_channel=LinkinPark',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
+                    _id: "61d3ccd8c3f17d0cc42e8e29",
+                    title: "3rd video",
+                },
+            },
+            {
+                title: "Numb - Linkin Park",
+                description: "This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=kXYiU_JCYtU&ab_channel=LinkinPark',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
+                    _id: "61d3ccd8c3f17d0cc42e8e29",
+                    title: "numb",
+                },
+            },
+            {
+                title: "Three Days Grace - I Hate Everything About You",
+                description: "2 This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=d8ekz_CSBVg&ab_channel=ThreeDaysGraceVEVO',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
+                    _id: "61d3ccd8c3f17d0cc42e8e29",
+                    title: "2nd video",
+                },
+            },
+            {
+                title: "Video 3",
+                description: "3 This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=kXYiU_JCYtU&ab_channel=LinkinPark',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
                 _id: "61d3ccd8c3f17d0cc42e8e29",
-                title: "2 videos",
-              },
+                title: "3rd video",
+                },
+            },
+            {
+                title: "Numb - Linkin Park",
+                description: "This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=kXYiU_JCYtU&ab_channel=LinkinPark',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
+                    _id: "61d3ccd8c3f17d0cc42e8e29",
+                    title: "numb",
+                },
+            },
+            {
+                title: "Three Days Grace - I Hate Everything About You",
+                description: "2 This is just a video. And if you want to know more about it etc...",
+                url:'https://www.youtube.com/watch?v=d8ekz_CSBVg&ab_channel=ThreeDaysGraceVEVO',
+                created:"2022-01-04T04:28:08.220Z",
+                discoveryCategory:{
+                    _id: "61d3ccd8c3f17d0cc42e8e29",
+                    title: "2nd video",
+                },
+            },
+            {
+                    title: "Video 3",
+                    description: "3 This is just a video. And if you want to know more about it etc...",
+                    url:'https://www.youtube.com/watch?v=kXYiU_JCYtU&ab_channel=LinkinPark',
+                    created:"2022-01-04T04:28:08.220Z",
+                    discoveryCategory:{
+                    _id: "61d3ccd8c3f17d0cc42e8e29",
+                    title: "3rd video",
+                    },
             }
-        ]
+        ];
+       
+        // const parser = new DOMParser();
+        // const video = parser.parseFromString((discoveries || [])[0]?.embed,  "text/html");
+        // console.log(video);
+
         return (
             <>
             <Header location={this.props.location}/>
             <Loader>
                 <Container>
-                    <div className="">
-                        {discoveries.demo}
+                    <div className="taninMedehui">
+                        <div className="contentHeader">
+                        </div>
+                        <div className="tanin">
+                            <img src="/images/logo2.png" />
+                            <div className="text">
+                                <h3> Танин мэдэхүй </h3>
+                                <p>
+                                    Энэхүү контентууд нь Odosury платформын өмч болно.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="videoContent">
+                            <ReactPlayer
+                                playing
+                                controls
+                                className="react-player"
+                                width={'100%'}
+                                height={'100%'}
+                                url={(discoveries || [])[this.state.index].url} />
+                        </div>
+                        <div className="videoDesc">
+                            <h5 className="title">
+                                {(discoveries || [])[this.state.index].title} 
+                            </h5>
+                            <p className="desc">
+                                {(discoveries || [])[this.state.index].description} 
+                            </p>
+                        </div>
+
+                        <div className="contents">
+                            {
+                                (discoveries || []).map((item, idx) => (
+                                    <div className="cardCont">
+                                        <div key={idx} className="card">
+                                            
+                                        </div>
+                                        <div className="title">
+                                            {item?.title}
+                                        </div>
+                                    </div>
+
+                                ))
+                            }
+                        </div>
                     </div>
-                    
-                    
                 </Container>
             </Loader>
             <Footer/>
